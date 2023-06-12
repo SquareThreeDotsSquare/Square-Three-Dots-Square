3ac6d8c053766f37568bec6a9c4120a9faff28f8
HYDR00GEN <ovy.hydrogen@gmail.com>
2023-06-12 23:30:33 +0200

    v3 archive

--- archive.js
+++ archive.js
@@ -1,4 +1,9 @@
-function toggleImage(element) {
-    var card = element.querySelector(".card");
+function toggleImageSX(element) {
+    var card = element.querySelector(".cardSX");
     card.style.display = card.style.display === "none" ? "block" : "none";
   }
+
+function toggleImageDX(element) {
+  var cardDX = element.querySelector(".cardDX");
+  cardDX.style.display = cardDX.style.display === "none" ? "block" : "none";
+}
